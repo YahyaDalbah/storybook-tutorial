a18ba321c9529641255865cfbd1f0fcc5f34d9b7
yahya dalbah <me@yahyadalbah.com>
2023-05-12 18:53:21 +0300

    addons

--- src/components/InboxScreen.stories.jsx
+++ src/components/InboxScreen.stories.jsx
@@ -32,18 +32,18 @@ export const Default = {
       ],
     },
   },
-play: async ({ canvasElement }) => {
-  const canvas = within(canvasElement);
-  // Waits for the component to transition from the loading state
-  await waitForElementToBeRemoved(await canvas.findByTestId('loading'));
-  // Waits for the component to be updated based on the store
-  await waitFor(async () => {
-    // Simulates pinning the first task
-    await fireEvent.click(canvas.getByLabelText('pinTask-1'));
-    // Simulates pinning the third task
-    await fireEvent.click(canvas.getByLabelText('pinTask-3'));
-  });
-},
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    // Waits for the component to transition from the loading state
+    await waitForElementToBeRemoved(await canvas.findByTestId('loading'));
+    // Waits for the component to be updated based on the store
+    await waitFor(async () => {
+      // Simulates pinning the first task
+      await fireEvent.click(canvas.getByLabelText('pinTask-1'));
+      // Simulates pinning the third task
+      await fireEvent.click(canvas.getByLabelText('pinTask-3'));
+    });
+  },
 };
 export const Error = {
   parameters: {
